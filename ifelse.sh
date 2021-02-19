f3857bb7efc6cef63d331aa4e0708b46f56fed1c
unknown <you@example.com>
2021-02-19 19:36:18 +0530

    Added: day between march20 and june20

--- ifelse.sh
+++ ifelse.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-min=a[0]
-max=a[0]
-n=5
-for ((i=0;i<n;i++))
- do
-    randomNumber=$((100 + $RANDOM%999))
-  echo $randomNumber
-done
-for ((i=1;i<n;i++))
-do
- if [ $min -gt a[i] ]
-    then
-        $min=a[i]
- if [ $max -lt a[i] ]
-     then
-         $max=a[i]
-done
-echo "Maximum value is:-" $max
-echo "Minimum value is:-" $min
+echo "Enter a day:"
+read day
+echo "Enter a month:"
+read month
 
+day=$day
+month=$month
+
+ if [$month -gt 4] && [$month -lt 6] && [$day -gt 0] && [$day -lt 31 ]
+
+  then
+       echo "True"
+  else
+       echo "False"
+fi
 

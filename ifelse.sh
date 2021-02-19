bc06ac9ba10559af455247df79add6bded2f6c8c
unknown <you@example.com>
2021-02-19 19:46:30 +0530

    Added: code of heads or tails

--- ifelse.sh
+++ ifelse.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-echo "Enter Year:"
-read y
-
-year=$y
-
-y=$(( $y % 4 ))
-if [ $y -eq 0 ]
-then
-    echo "$year is Leap Year!"
+#!/bin/bash -x
+flip=$(($(($RANDOM%10))%2))
+if [ $flip -eq 1 ]
+  then
+    echo "heads"
 else
-    echo "$year is not a Leap Year!"
+     echo "tails"
 fi
+
+
 
 

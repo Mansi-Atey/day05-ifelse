9318db09cd54b52bd2d48c64213e714d2a35cc37
unknown <you@example.com>
2021-02-19 19:31:08 +0530

    Added: code of min max of random number

--- ifelse.sh
+++ ifelse.sh
@@ -1,1 +1,22 @@
-echo "problems on if else"
+#!/bin/bash
+min=a[0]
+max=a[0]
+n=5
+for ((i=0;i<n;i++))
+ do
+    randomNumber=$((100 + $RANDOM%999))
+  echo $randomNumber
+done
+for ((i=1;i<n;i++))
+do
+ if [ $min -gt a[i] ]
+    then
+        $min=a[i]
+ if [ $max -lt a[i] ]
+     then
+         $max=a[i]
+done
+echo "Maximum value is:-" $max
+echo "Minimum value is:-" $min
+
+
